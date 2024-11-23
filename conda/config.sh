1b114f8f57feb88293493662c1c8b37c45f297c4
Gavin Gao <cggos@outlook.com>
2024-11-23 11:23:22 +0800

    update conda_envs

---
+++ conda/config.sh
@@ -0,0 +1,6 @@
+conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/free/
+conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/main/
+conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud/pytorch/
+conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud/pytorch/linux-64/
+conda config --set show_channel_urls yes
+
