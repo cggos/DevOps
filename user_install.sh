7ff26ae20ceeb3fadd4c8208251cf869a467aa54
Gavin Gao <cggos@outlook.com>
2025-07-27 23:28:41 +0800

    add user_install.sh

---
+++ user_install.sh
@@ -0,0 +1,91 @@
+sudo apt update
+
+sudo apt install vim curl wget 
+
+sudo apt install zsh tmux
+
+# oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+
+##### Coding
+sudo apt install build-essential ninja-build ccache git cmake cmake-curses-gui rabbitvcs-* 
+
+sudo apt install clang-format
+
+sudo apt install python3-pip
+
+sudo python3 -m pip install black
+
+# VS Code (Deb)
+# JetBrains: toolbox, pycharm, clion
+
+
+##### OA
+
+# IM
+sudo apt install fcitx fcitx-googlepinyin
+
+# TODO
+# sudo snap install microsoft-todo-unofficial
+
+# SSH
+sudo apt install openssh-server
+
+# FTP
+sudo apt install filezilla
+
+
+# google-chrome (Deb) 
+
+# synology-drive-client (Deb)
+
+
+##### Docs
+
+# OB (or AppImage)
+sudo snap install obsidian --classic
+
+# Typora (Deb)
+
+# WPS (Deb)
+
+# OneNote
+# sudo snap install p3x-onenote
+# sudo snap install onenote-desktop --beta
+
+
+##### Media
+
+# Screenshot
+sudo apt install flameshot # command: flameshot gui
+
+# PDF
+sudo apt install okular
+
+# Diagram
+# xmind (Deb)
+# draw.io (Deb)
+
+# Image Tools
+sudo apt install imagemagick gimp
+# sudo apt install chafa
+
+sudo apt install ffmpeg vlc
+# sudo apt install mpv
+
+# sudo snap install blender
+
+
+##### Social
+
+sudo apt install iptux
+
+# ToDesk (Deb)
+
+# WeChat (Deb)
+
+# TencentMeeting (Deb)
+
+# sudo snap install telegram-desktop
+
