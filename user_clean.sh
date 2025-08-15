3eee30f4df65fffc580282ce430559c3c75b3138
Gavin Gao <cggos@outlook.com>
2025-08-15 17:40:43 +0800

    update user_clean.sh

--- user_clean.sh
+++ user_clean.sh
@@ -1,8 +1,15 @@
 #!/usr/bin env bash
 
 rm -fr ~/.cache 
-# ~/.local/share ~/.local/share/applications
+rm -fr ~/.local/share/Trash
+rm -fr /var/cache 
+rm -fr /var/log
 
-rm -fr /var/cache /var/log
+# rm -fr ~/.config
+
+# ~/.bashrc
+# ~/.zshrc
+
+# ~/.local/share 
+# ~/.local/share/applications
 # /usr/share/applications
-
